308e11ff5176d80d6ab2637980409008be2ed7af
anitanasimi <106728239+anitanasimi@users.noreply.github.com>
2023-04-12 02:24:59 -0700

    last update

--- src/areas/post/controllers/post.controller.ts
+++ src/areas/post/controllers/post.controller.ts
@@ -1,38 +1,124 @@
 import { Request, Response, NextFunction, Router } from "express";
 import IController from "../../../interfaces/controller.interface";
 import IPostService from "../services/IPostService";
-import { post, posts } from "../../../model/fakeDB";
-
+import { ensureAuthenticated } from "../../../middleware/authentication.middleware";
+import { PostViewModel } from "../views/post.viewmodel";
+import PostNotFoundException from "../../../exceptions/PostNotFoundException";
+import IUser from "../../../interfaces/user.interface";
+declare global {
+  namespace Express {
+    interface User extends IUser {}
+  }
+}
 class PostController implements IController {
   public path = "/posts";
   public router = Router();
+  postService: IPostService;
 
   constructor(postService: IPostService) {
     this.initializeRoutes();
+    this.postService = postService;
   }
 
   private initializeRoutes() {
-    this.router.get(this.path, this.getAllPosts);
-    this.router.get(`${this.path}/:id`, this.getPostById);
-    this.router.get(`${this.path}/:id/delete`, this.deletePost);
-    this.router.post(`${this.path}/:id/comment`, this.createComment);
-    this.router.post(`${this.path}`, this.createPost);
+    this.router.get(this.path, ensureAuthenticated, this.getAllMyPosts);
+    this.router.get(`${this.path}/following`, ensureAuthenticated, this.getAllFollowerPosts);
+    this.router.get(`${this.path}/:id`, ensureAuthenticated, this.getPostById);
+    this.router.post(`${this.path}/:id/delete`, ensureAuthenticated, this.deletePost);
+    this.router.post(`${this.path}/:id/comment`, ensureAuthenticated, this.createComment);
+    this.router.get(`${this.path}/:id/like`, ensureAuthenticated, this.likePostById);
+    this.router.post(`${this.path}`, ensureAuthenticated, this.createPost);
+    this.router.get(`${this.path}/:id/invalidPost`, ensureAuthenticated, this.showInvalidPost);
   }
 
-  // 🚀 This method should use your postService and pull from your actual fakeDB, not the temporary posts object
-  private getAllPosts = (_: Request, res: Response) => {
-    res.render("post/views/posts", { posts });
+  private getAllMyPosts = async (req: Request, res: Response) => {
+    const user = req.user;
+    const posts = await this.postService.getAllPosts(user.id);
+    const postsFormatted = await Promise.all(
+      posts.map(async (post) => {
+        const postViewModel = new PostViewModel();
+        await postViewModel.init(post, user.id);
+        return postViewModel;
+      })
+    );
+
+    res.render("post/views/posts", { posts: postsFormatted });
+  };
+  private getAllFollowerPosts = async (req: Request, res: Response) => {
+    const user = req.user;
+    const followers = await this.postService.getUserFollowers(user.id);
+    const posts = await this.postService.getAllPostsByUserFollowers(followers);
+
+    const postsFormatted = await Promise.all(
+      posts.map(async (post) => {
+        const postViewModel = new PostViewModel();
+        await postViewModel.init(post, user.id);
+        return postViewModel;
+      })
+    );
+
+    res.render("post/views/posts", { posts: postsFormatted });
   };
 
-  // 🚀 This method should use your postService and pull from your actual fakeDB, not the temporary post object
-  private getPostById = async (request: Request, res: Response, next: NextFunction) => {
-    res.render("post/views/post", { post });
+  private getPostById = async (req: Request, res: Response, next: NextFunction) => {
+    const id = req.params.id;
+    const userId = req.user.id;
+    try {
+      const post = await this.postService.findById(id);
+      if (!post) {
+        res.redirect(`/posts/${id}/invalidPost`);
+        next(new PostNotFoundException(id));
+      } else {
+        const postFormatted = new PostViewModel();
+        await postFormatted.init(post, userId);
+        res.render("post/views/post", { post: postFormatted });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  private showInvalidPost = async (req: Request, res: Response) => {
+    const id = req.params.id;
+    res.render("post/views/invalidPost", { id });
   };
 
-  // 🚀 These post methods needs to be implemented by you
-  private createComment = async (req: Request, res: Response, next: NextFunction) => {};
-  private createPost = async (req: Request, res: Response, next: NextFunction) => {};
-  private deletePost = async (req: Request, res: Response, next: NextFunction) => {};
+  private likePostById = async (req: Request, res: Response, next: NextFunction) => {
+    const user = req.user;
+    const postId = req.params.id;
+    const post = new PostViewModel();
+    await post.init(await this.postService.findById(postId), user.id);
+    const likedPost = post.userLiked;
+    likedPost ? await this.postService.unlikePost(postId, user.id) : await this.postService.likePost(postId, user.id);
+    res.redirect("back");
+  };
+
+  private createComment = async (req: Request, res: Response, next: NextFunction) => {
+    const user = req.user;
+    const postId = req.params.id;
+    const message: string = req.body.commentText;
+    if (message.trim() !== "") {
+      await this.postService.addCommentToPost(user.id, message, postId);
+    }
+    res.redirect("back");
+  };
+
+  private createPost = async (req: Request, res: Response, next: NextFunction) => {
+    const user = req.user;
+    const message: string = req.body.postText;
+    if (message.trim() !== "") {
+      await this.postService.addPost(message, user.id);
+    }
+    res.redirect("back");
+  };
+  private deletePost = async (req: Request, res: Response, next: NextFunction) => {
+    const postId = req.params.id;
+    const user = req.user;
+    const post = await this.postService.findById(postId);
+    if (post.creatorId === user.id) {
+      await this.postService.deletePost(postId);
+    }
+    res.redirect("back");
+  };
 }
 
 export default PostController;
--- src/areas/post/services/Post.service.ts
+++ src/areas/post/services/Post.service.ts
@@ -1,27 +1,99 @@
+import IFollower from "../../../interfaces/follower.interface";
 import IPost from "../../../interfaces/post.interface";
+import DBClient from "../../../PrismaClient";
 import IPostService from "./IPostService";
 
-// ❗️ Implement this class much later, once everything works fine with your mock db
 export class PostService implements IPostService {
-  addPost(post: IPost, username: string): void {
-    // 🚀 Implement this yourself.
-    throw new Error("Method not implemented.");
+  readonly _db: DBClient = DBClient.getInstance();
+  async addPost(message: string, userId: string): Promise<void> {
+    await this._db.prisma.post.create({
+      data: {
+        message: message,
+        creatorId: userId,
+      },
+    });
   }
-  getAllPosts(username: string): IPost[] {
-    // 🚀 Implement this yourself.
-    throw new Error("Method not implemented.");
+  async deletePost(postId: string): Promise<void> {
+    await this._db.prisma.comment.deleteMany({
+      where: {
+        postId,
+      },
+    });
+    await this._db.prisma.like.deleteMany({
+      where: {
+        postId,
+      },
+    });
+    await this._db.prisma.post.delete({
+      where: {
+        id: postId,
+      },
+    });
   }
-  findById(id: string): IPost {
-    // 🚀 Implement this yourself.
-    throw new Error("Method not implemented.");
+  async getAllPosts(userId: string): Promise<IPost[]> {
+    return await this._db.prisma.post.findMany({
+      where: {
+        creatorId: userId,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
   }
-  addCommentToPost(message: { id: string; createdAt: string; userId: string; message: string }, postId: string): void {
-    // 🚀 Implement this yourself.
-    throw new Error("Method not implemented.");
+  async getAllPostsByUserFollowers(followers: IFollower[]): Promise<IPost[]> {
+    const allFollowersPosts: IPost[] = [];
+    for await (const follower of followers) {
+      const followersPosts = await this.getAllPosts(follower.followingId);
+      allFollowersPosts.push(...followersPosts);
+    }
+    return allFollowersPosts;
   }
-
-  sortPosts(posts: IPost[]): IPost[] {
-    // 🚀 Implement this yourself.
-    throw new Error("Method not implemented.");
+  async getUserFollowers(userId: string): Promise<IFollower[]> {
+    return await this._db.prisma.follower.findMany({
+      where: {
+        userId,
+      },
+    });
+  }
+  async sortPosts(): Promise<IPost[]> {
+    return await this._db.prisma.post.findMany({
+      orderBy: {
+        createdAt: "asc",
+      },
+    });
+  }
+  async findById(id: string): Promise<IPost> {
+    return await this._db.prisma.post.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+  async likePost(postId: string, userId: string): Promise<void> {
+    await this._db.prisma.like.create({
+      data: {
+        postId,
+        userId,
+      },
+    });
+  }
+  async unlikePost(postId: string, userId: string): Promise<void> {
+    await this._db.prisma.like.delete({
+      where: {
+        postId_userId: {
+          postId,
+          userId,
+        },
+      },
+    });
+  }
+  async addCommentToPost(creatorId: string, message: string, postId: string): Promise<void> {
+    await this._db.prisma.comment.create({
+      data: {
+        creatorId,
+        message,
+        postId,
+      },
+    });
   }
 }
